3559e12591046f5d86b8a187cb616000ffa1a4b3
eugenesqr <eskurihin@gmail.com>
2022-01-28 10:04:46 +0300

    initial click setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='storcom',
+    version='0.0.1',
+    py_modules=['storage_commander'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'storcom = storage_commander:storcom',
+        ],
+    },
+)
---
+++ storage_commander.py
@@ -0,0 +1,5 @@
+import click
+
+@click.command()
+def storcom():
+    print('Hello from storage commander')
