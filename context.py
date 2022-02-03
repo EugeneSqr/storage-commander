68cf60df3b005dbc2aab369d79103755ed408a8d
eugenesqr <eskurihin@gmail.com>
2022-02-03 14:19:37 +0300

    pass context to subcommands

--- context.py
+++ context.py
@@ -4,21 +4,24 @@ from pathlib import Path
 
 import click
 
+_CONTEXT_FILE = '.context'
+
 @click.group()
 def context():
     '''Which environment, storage, service or owner to use.'''
 
-# TODO: load context somewhere at the beginning and propagate to all other commands
+# TODO: add choise options for storage and environment
+# TODO: context show invoke group without command https://click.palletsprojects.com/en/8.0.x/commands/#group-invocation-without-command
 @context.command()
 @click.option('--environment', help='Environment where a storage is deployed.')
 @click.option('--storage', help='Storage type name.')
 @click.option('--service', help='Name of the service to which files belong.')
 @click.option('--user', help='Name of the user which owns the files.')
-def use(**kwargs):
+@click.pass_context
+def use(context, **kwargs):
     '''Set context for subsequent operations.'''
-    context = Context.load()
-    context.update(**kwargs)
-    print(context.save())
+    context.storcom_context.update(**kwargs)
+    print(context.storcom_context.save())
 
 class Context():
     def __init__(self, **kwargs):
@@ -29,16 +32,16 @@ class Context():
         return self
 
     def save(self):
-        with open('.env', 'w+') as f:
+        with open(_CONTEXT_FILE, 'w+') as f:
             for key, value in self.__dict__.items():
                 f.write(f'{key}={value}{os.linesep}')
         return self
 
     @classmethod
     def load(cls):
-        if not Path('.env').is_file():
+        if not Path(_CONTEXT_FILE).is_file():
             return Context()
-        with open('.env') as f:
+        with open(_CONTEXT_FILE) as f:
             return Context(**dict(line.rstrip().split('=') for line in f))
 
     def __repr__(self):
--- storage_commander.py
+++ storage_commander.py
@@ -1,9 +1,11 @@
 import click
 
-from context import context
+from context import context as context_command, Context
 
 @click.group()
-def storcom():
+@click.pass_context
+def storcom(context):
     '''A unified way of working with all your storages.'''
+    context.storcom_context = Context.load()
 
-storcom.add_command(context)
+storcom.add_command(context_command)
