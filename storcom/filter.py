869ded9b6d389cd41fbb29e95ab7ebf04b8300d7
eugenesqr <eskurihin@gmail.com>
2023-03-26 09:27:52 +0300

    handle some filter params edge cases

--- storcom/filter.py
+++ storcom/filter.py
@@ -5,6 +5,12 @@ FCC_FILTER_FIELDS = ['batch']
 def to_fcc_qs_params(filters: Dict[str, str]) -> Dict[str, str]:
     return dict(_to_fcc_qs_param(k, v) for k, v in filters.items() if v and k in FCC_FILTER_FIELDS)
 
-def _to_fcc_qs_param(key: str, value: str) -> Tuple[str, str]:
-    split_value = value.split()
-    return key, split_value[1].strip('\'"')
+def _to_fcc_qs_param(key: str, filter_value: str) -> Tuple[str, str]:
+    split_filter_value = filter_value.split(' ', 1)
+    param_operator, param_value = 'eq', ''
+    if len(split_filter_value) == 1:
+        param_value = split_filter_value[0]
+    else:
+        param_operator, param_value = split_filter_value
+        param_operator = param_operator.lower()
+    return key, param_value.strip('\'"')
--- storcom/test_filter.py
+++ storcom/test_filter.py
@@ -8,14 +8,24 @@ def test_supported_fcc_filters() -> None:
 def test_to_fcc_qs_params_ignores_unsupported_filters() -> None:
     assert not flt.to_fcc_qs_params({'smth': 'eq 2'})
 
-@pytest.mark.parametrize('value', ['eq test', 'eq "test"', "eq 'test'"])
-def test_to_fcc_qs_params_supports_eq_text_values(value: str) -> None:
-    assert flt.to_fcc_qs_params({'batch': value}) == {'batch': 'test'}
+def test_to_fcc_qs_params_ignores_empty_filters() -> None:
+    assert not flt.to_fcc_qs_params({'batch': ''})
 
-@pytest.mark.parametrize('value', ['eq 42', 'eq "42"', "eq '42'"])
-def test_to_fcc_qs_params_supports_eq_number_values(value: str) -> None:
-    assert flt.to_fcc_qs_params({'batch': value}) == {'batch': '42'}
+@pytest.mark.parametrize('actual,expected', [
+    ('eq test', 'test'),
+    ('eq "0"', '0'),
+    ("eq 'this is test'", 'this is test'),
+])
+def test_to_fcc_qs_params_eq_operator(actual: str, expected: str) -> None:
+    assert flt.to_fcc_qs_params({'batch': actual}) == {'batch': expected}
 
-# TODO: none tests line {'batch': None}
-# TODO: add default eq filter tests
-# TODO: case tests for EQ vs eq
+@pytest.mark.parametrize('actual,expected', {
+    ('test', 'test'),
+    ('"0"', '0'),
+    ("eq 'this is test'", 'this is test'),
+})
+def test_to_fcc_qs_params_default_eq_operator(actual: str, expected: str) -> None:
+    assert flt.to_fcc_qs_params({'batch': actual}) == {'batch': expected}
+
+def test_to_fcc_qs_params_operator_case_insensitive() -> None:
+    assert flt.to_fcc_qs_params({'batch': 'EQ 3'}) == {'batch': '3'}
